92f9e20bb652914acdb4b2f9fec14de05ee102c1
jeanvicentes <jeanvicente077@gmail.com>
2021-08-06 16:43:52 +0000

    adicionando codigos de canaltexto

--- include/canaltexto.h
+++ include/canaltexto.h
@@ -1,10 +1,30 @@
 #ifndef CANALTEXTO_H
 #define CANALTEXTO_H
+#include <iostream>
+#include <vector>
+#include "mensagem.h"
+#include "usuario.h"
 
-class CanalTexto{
-	/*
-		TODO implementação dos atributos e métodos
-	*/
+enum canalType {
+  TEXT,
+  VOICE
 };
+
+/** Classe que representa canal */
+class canal {
+  protected:
+    std::string nome; /**< Nome do canal */
+  public:
+    canal(std::string _nome) { nome =_nome; } /**< Construtor parametrizado de canal */
+    virtual ~canal() {} /**< Destrutor de canal */
+
+    std::string getnome() { return nome; } /**< Retorna o nome */
+
+    virtual canalType getType() = 0; /**< Método virtual que retorna o tipo do canal */
+    virtual void addMensagem (mensagem newmensagem) = 0; /**< Método virtual que adiciona uma mensagem */
+    virtual std::string printMensagem(std::vector<usuario> usuarios) = 0; /**< Método virtual que retorna as mensagens formatadas na string */
+    virtual std::vector<mensagem> getMensagens() = 0; /**< Método virtual que retorna as mensagens */
+};
+
 
 #endif
