7fb4f6702d18c553afc09b576d51fcb24ba7a69c
jeanvicentes <jeanvicente077@gmail.com>
2021-08-06 18:53:45 +0000

    adicionando comentarios em usario.h

--- include/usuario.h
+++ include/usuario.h
@@ -1,28 +1,27 @@
 #ifndef Usuario_H
 #define Usuario_H
-
+//**< biblioteca padrão */
 #include <iostream>
 
 using namespace std;
 
-
+/** Classe que representa o usuário do sistema */
 class Usuario {
   private:
-    
-    int id; 
-    string nome; 
-    string email; 
-    string senha; 
+    int id; /**< ID do usuário */
+    std::string nome; /**< Nome do usuário */
+    std::string email; /**< Email do usuário */
+    std::string senha; /**< Senha do usuário */
 
   public:
-    Usuario(); //construtor
-    Usuario(const int pid, const string pnome, const string pemail, const string psenha); 
-    ~Usuario(); 
+    Usuario(); /**< Construtor do usuário */
+    Usuario(const int _id, const std::string _nome, const std::string _email, const std::string _senha); /**< Construtor parametrizado do usuário */
+    ~Usuario();/**< Destrutor do usuário */
 
-    int getId();
-    string getNome(); 
-    string getEmail(); 
-    string getSenha();
+    int getId(); /**< pega o id do usuário */
+    std::string getNome(); /**< pega o nome do usuário */
+    std::string getEmail(); /**< pega o email do usuário */
+    std::string getsenha(); /**< pega a senha do usuário */
 };
 
 #endif
