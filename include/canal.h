0ba0da4eb065439f9a42fe50a9d48c9f4860abd9
jeanvicentes <jeanvicnete077@gmail.com>
2021-08-09 20:19:41 +0000

    adicionando constructor de canal.h

--- include/canal.h
+++ include/canal.h
@@ -1,1 +1,23 @@
+#ifndef CHANNEL_H
+#define CHANNEL_H
+#include <iostream>
+#include <vector>
+#include "mensagem.h"
+#include "usuario.h"
 
+enum channelType {
+  TEXTO,
+
+ class Canal {
+  protected:
+    std::string nome; /**< Nome do canal */
+  public:
+    canal(std::string _nome) { nome = _nome; } /**< Construtor parametrizado de canal */
+    virtual ~canal() {} /**< Destrutor de canal */
+
+    std::string getNome() { return nome; } /**< Retorna o nome */
+
+  };
+
+
+#endif   
