2793be79b3cdf1ec903fd24a101132f5216ab743
jeanvicentes <jeanvicentes077@gmail.com>
2021-08-09 23:39:24 +0000

    atualizando canal.h

--- include/canal.h
+++ include/canal.h
@@ -5,7 +5,7 @@
 #include "mensagem.h"
 #include "usuario.h"
 
-enum channelType {
+enum tipocanal {
   TEXTO,
 
  class Canal {
@@ -16,7 +16,11 @@ enum channelType {
     virtual ~canal() {} /**< Destrutor de canal */
 
     std::string getNome() { return nome; } /**< Retorna o nome */
-
+  
+    virtual tipocanal gettipo() = 0; 
+    virtual void addMensagem(mensagem novaMensagem) = 0; 
+    virtual std::string printMensages(std::vector<usuario> usuarios) = 0; 
+    virtual std::vector<mensagem> getMensagens() = 0; 
   };
 
 
